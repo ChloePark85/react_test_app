ad2b330e40abfe2caf8f0215673a98882dccba6b
ChloePark85 <hapark85@gmail.com>
2021-01-13 22:42:41 +0900

    Update my_drawer.js

--- src/my_drawer.js
+++ src/my_drawer.js
@@ -2,8 +2,19 @@ import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import {StyleSheet, ScrollView, Image, View, Text, Button} from 'react-native';
 import Logo from './assets/pics/home_icon.png';
+import { CommonActions } from '@react-navigation/native'
 
 class SideDrawer extends Component {
+
+    navigateToScreen = (route) => () => {
+        this.props.navigation.dispatch(
+            CommonActions.navigate({
+                name: route,
+                params: {},
+            })
+        )
+    }
+
     render() {
         return (
             <View style={StyleSheet.container}>
